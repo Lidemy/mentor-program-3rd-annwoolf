a7617987e61fea9caffd47448cc9f6d2858512d3
ann800213 <ann800213@gmail.com>
2019-05-04 16:04:34 +0800

    hw1

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,17 @@
+function print(n) {
+  let printStar = '';
+  for (let j = 1; j <= n; j += 1) {
+    printStar += '*';
+  }
+  return printStar;
+}
+
 function stars(n) {
-  console.log(n);
+  const arr = [];
+  for (let i = 1; i <= n; i += 1) {
+    arr.push(print(i));
+  }
+  return arr;
 }
 
 module.exports = stars;
