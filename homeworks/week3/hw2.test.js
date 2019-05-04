270d888ea0a5f4620381b8319b4bc0f3269913c5
ann800213 <ann800213@gmail.com>
2019-05-05 02:36:19 +0800

    hw1-hw4 test

--- homeworks/week3/hw2.test.js
+++ homeworks/week3/hw2.test.js
@@ -4,4 +4,10 @@ describe('hw2', () => {
   it('should return correct answer when str = nick', () => {
     expect(alphaSwap('nick')).toBe('NICK');
   });
+  it('should return correct answer when str = nick', () => {
+    expect(alphaSwap('Nick')).toBe('nICK');
+  });
+  it('should return correct answer when str = nick', () => {
+    expect(alphaSwap(',hEllO123')).toBe(',HeLLo123');
+  });
 });
--- homeworks/week3/hw3.test.js
+++ homeworks/week3/hw3.test.js
@@ -4,4 +4,10 @@ describe('hw3', () => {
   it('should return correct answer when n = 1', () => {
     expect(isPrime(1)).toBe(false);
   });
+  it('should return correct answer when n = 1', () => {
+    expect(isPrime(37)).toBe(true);
+  });
+  it('should return correct answer when n = 1', () => {
+    expect(isPrime(38)).toBe(false);
+  });
 });
