15da4f267e3c4708f2727d7058879a6734ff5a25
ann800213 <ann800213@gmail.com>
2019-05-05 00:58:02 +0800

    hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,17 @@
 function isPrime(n) {
-  console.log(n);
+  if (n === 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
+
+console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(10));
+console.log(isPrime(37));
+console.log(isPrime(38));
 
 module.exports = isPrime;
