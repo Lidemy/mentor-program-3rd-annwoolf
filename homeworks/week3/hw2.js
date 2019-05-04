221dafc0422d9f70f527c4a8861f72945fa0dc02
ann800213 <ann800213@gmail.com>
2019-05-05 00:32:33 +0800

    hw2

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,19 @@
 function alphaSwap(str) {
-  console.log(str);
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      result += str[i].toUpperCase();
+    } else if (str[i] >= 'A' && str[i] <= 'Z') {
+      result += str[i].toLowerCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
+
+console.log(alphaSwap('nick'));
+console.log(alphaSwap('Nick'));
+console.log(alphaSwap(',hEllO123'));
 
 module.exports = alphaSwap;
